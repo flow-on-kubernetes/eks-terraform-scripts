8575e88ed87a27532478e4a1025b2d244afc8678
sachingade20 <sachin.gade@opcito.com>
2021-04-30 16:32:44 +0530

    Added eks-vpc

---
+++ eks-vpc/variables.tf
@@ -0,0 +1,52 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "cluster-name" {
+  default = "flow17-aks"
+}
+variable "cluster-version" {
+  default = "1.17"
+}
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
+
+  default = [
+    "202127779570"
+  ]
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+  ]
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      userarn  = "arn:aws:iam::202127779570:user/opcitosaas"
+      username = "opcitosaas"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::202127779570:user/SmtpEFUser"
+      username = "SmtpEFUser"
+      groups   = ["system:masters"]
+    }
+  ]
+}
